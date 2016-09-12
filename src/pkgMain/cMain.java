f738d5b68f7fe37bf98791844af6f3a850f8bc88
r <r@10.180.66.67>
2016-09-12 16:18:40 -0400

    first commit

--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -3,9 +3,16 @@ package pkgMain;
 public class cMain {
 
 	public static void main(String[] args) {
+		
+		
+		/*
+		 * My github account name:GraceWang
+		 */
 
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
 	}
+
+	
 
 }
